e854b6dc0054d2942c13cc6db948e0b95c85215e
Ilia Konnov <iliakonnov@gmail.com>
2016-11-29 16:29:40 +0300

    Не добавил в 5e11f95

--- script.js
+++ script.js
@@ -1,131 +1,22 @@
 var results = {};
 
-// ================================================= Moonwalk
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if (details.tabId != -1) {
-            var formData = {
-                video_token: details.requestBody.formData["video_token"][0],
-                access_key: details.requestBody.formData["access_key"][0],
-                mw_domain_id: details.requestBody.formData["mw_domain_id"][0]
-            };
-            
-            $.post({
-                type: "POST",
-                url: details.url,
-                data: formData,
-                dataType: "json",
-                headers: {
-                    "X-Data-Pool": "Stream",
-                    "X-Requested-With": "XMLHttpRequest"
-                },
-                success: function(data) {
-                    //data = JSON.parse(xhr.responseText);
+// Loading sources
+["moonwalk", "mp4_flv", "youtube"].forEach(function (item, i, arr) {
+    var scriptEl = document.createElement('script');
+    scriptEl.src = chrome.extension.getURL("sources/"+item+".js");
+    scriptEl.addEventListener('load', function( data, textStatus, jqxhr ) {
+        log('Source "'+item+'" loaded')
+    }, false);
+    document.head.appendChild(scriptEl);
+});
 
-                    var url = data["mans"]["manifest_m3u8"];
-                    var views = chrome.extension.getViews({
-                        type: "popup"
-                    });
-
-                    addResult(details.tabId, {
-                        id: url.split('/')[4],
-                        file: url,
-                        kodi: url,
-                        size: -1
-                    });
-                    setIcon(details.tabId);
-                }
-            });
-        }
-    },
-    {urls: ["*://*/*new_session"]},
-    ["requestBody"]
-);
-
-// ================================================= flv/mp4
-chrome.webRequest.onHeadersReceived.addListener(
-    function(details) {
-        if (details.tabId != 1) {
-            var url = details.url;
-            var length = details.responseHeaders.filter(function( obj ) {
-                return obj.name == "Content-Length";
-            })[0]["value"];
-            var filename = url.split("/").pop().split(".");
-            var extension = filename.pop();
-            filename = filename[0];
-
-            var result = getResult(details.tabId);
-            if (Object.keys(result).length == 0) {  // Result for rhis tab is empty
-                if (result.size != -1) {  // Not moonwalk or youtube
-                    var size = (result.size == undefined) ? -1 : result.size;
-                    if (size < length) {  // Filter ads
-                        addResult(details.tabId, {
-                            id: filename+extension+length,
-                            file: url,
-                            kodi: url,
-                            size: length
-                        });
-                        setIcon(details.tabId);
-                    }
-                }
-            }
-        }
-    },
-    {urls: ["*://*/*.mp4", "*://*/*.mp4?*",
-            "*://*/*.flv", "*://*/*.flv?*"]},
-    ["responseHeaders"]
-);
-
-// ================================================= Youtube-playlist
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var playlistId = details.url;  // https://youtube.com/playlist?a=bcd&list=PLAYLISTID&e=fgh
-        playlistId = playlistId.split("?")[1];  // a=bcd&list=PLAYLISTID&e=fgh
-        playlistId = playlistId.split("&");  // ["a=bcd", "list=PLAYLISTID", "e=fgh"]
-        for (i in playlistId) {
-            if ( playlistId[i].split("=")[0] == "list" ) {
-                playlistId = playlistId[i].split("=")[1];
-                break;
-            }
-        }
-        addResult(details.tabId, {
-            id: "yt-playlist-" + playlistId,
-            file: details.url,
-            kodi: "plugin://plugin.video.youtube/play/?order=default&playlist_id=" + playlistId,
-            size: -1
-        });
-        setIcon(details.tabId);
-    },
-    {urls: ["*://youtube.com/playlist?*", "*://www.youtube.com/playlist?*"]},
-    []
-);
-
-// ================================================= Youtube-video
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var videoId = details.url;  // https://youtube.com/watch?a=bcd&v=VIDEOID&e=fgh
-        videoId = videoId.split("?")[1];  // a=bcd&v=VIDEOID&e=fgh
-        videoId = videoId.split("&");  // ["a=bcd", "v=VIDEOID", "e=fgh"]
-        for (i in videoId) {
-            if ( videoId[i].split("=")[0] == "v" ) {
-                videoId = videoId[i].split("=")[1];
-                break;
-            }
-        }
-        addResult(details.tabId, {
-            id: "yt-video-" + videoId,
-            file: details.url,
-            kodi: "plugin://plugin.video.youtube/play/?video_id=" + videoId,
-            size: -1
-        });
-        setIcon(details.tabId);
-    },
-    {urls: ["*://youtube.com/watch?*", "*://www.youtube.com/watch?*"]},
-    []
-);
-
-// ================================================= Another
 function addResult(tabId, result) {
+    if (Object.keys(result).length != 0) {
+        log({
+            tabId: tabId,
+            result: result
+        });
+    }
     return results[tabId] = result;
 }
 
@@ -208,3 +99,4 @@ chrome.runtime.onInstalled.addListener( function(details) {
 function log(data) {
     chrome.extension.getBackgroundPage().console.log(data);
 }
+
